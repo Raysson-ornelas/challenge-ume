7e82d7ca9db1bbb56706d379a7eaae8720c7d906
Raysson Ornelas <rayssonornelas2006@gmail.com>
2021-12-03 23:00:43 -0300

    fea: created table layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,50 @@
 import type { NextPage } from 'next'
-import { Flex, Heading } from '@chakra-ui/react'
+import { Grid, Box, Input, Button, VStack, Stack} from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background="gray.100" justifyContent="center">
-        <Heading mb={6}>Teste</Heading>
-      </Flex>
-    </Flex>
+    <>
+    <VStack bg="gray.300" height="100vh">
+      <Box bg="#02054B" w="50vw" height="90vh" mt={10}>
+        <VStack height="70vh" alignItems="center" justifyContent="center">
+          <Grid templateColumns='repeat(5,1fr)' gap={1}>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+            <Box w='8vw' h='10vh' bg='blue.500'/>
+          </Grid>
+        </VStack>
+        <VStack spacing={5} height="18vh" alignItems="center" justifyContent="center">
+            <Input  width="20vw" mb={5} type="text" size="md" color="white"></Input>
+            <Stack direction="row">
+              <Button size="lg" variant="outline" color="white" _hover={{borderColor:'blue.500'}}>Reset</Button>
+              <Button size="lg" variant="solid" color='black' _hover={{bg:'blue.100'}}>Subimit</Button>
+            </Stack>
+        </VStack>
+      </Box>
+    </VStack>
+    </>
   )
 }
 
